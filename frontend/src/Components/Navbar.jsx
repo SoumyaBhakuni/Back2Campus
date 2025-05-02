7127738c3a07c9635755562a8c357e3bbe0d0999
atharvarajsinha <sinhaatharva@gmail.com>
2025-05-02 15:21:16 +0530

    role -> status

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     if (user) {
       setIsAuthenticated(true);
       const parsedUser = JSON.parse(user);
-      setUserRole(parsedUser.role);
+      setUserRole(parsedUser.status);
     }
   }, []);
 
